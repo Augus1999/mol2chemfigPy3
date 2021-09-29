23fc2c3d39322cfa2ae96af9f204ed79705e3f6f
SUENO Omozawa <39725660+Augus1999@users.noreply.github.com>
2021-09-29 15:45:20 +0100

    Update common.py

--- mol2chemfigPy3/common.py
+++ mol2chemfigPy3/common.py
@@ -28,7 +28,7 @@ For more information, type '%(progname)s --help'.
 
 _help_blurb = '''
 %(progname)s v. %(version)s,  originally by Eric Brefo-Mensah and Michael Palmer
-re-write in python by Nianze A. TAO (Omozawa SUENO) in 2021
+re-write in python3 by Nianze A. TAO (Omozawa SUENO) in 2021
 %(progname)s generates chemfig code from molfiles. Usage example:
 
 %(progname)s --angle=45 --aromatic-circles somefile.mol
