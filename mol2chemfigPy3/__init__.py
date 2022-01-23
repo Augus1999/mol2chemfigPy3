2ff8f81609a12d144cd629782d6b65bb30e93795
SUENO Omozawa <39725660+Augus1999@users.noreply.github.com>
2022-01-23 12:27:32 +0000

    update setup.py

--- mol2chemfigPy3/__init__.py
+++ mol2chemfigPy3/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 # make this a python package
+"""
+a python 3 version of mol2chemfig.
+mol2chemfig generates chemfig code from molfiles.
+"""
 from .processor import process
 from .common import program_version
 from .addin import mol2chemfig
--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 # Author: Nianze A. TAO (Omozawa SUENO)
 import os
+import pkg_resources
 from shutil import rmtree
 from setuptools import setup, find_packages
-from mol2chemfigPy3 import __version__
 
 
 with open("README.md", mode="r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+version = pkg_resources.require('mol2chemfigPy3')[0].version
+
 setup(
     name='mol2chemfigPy3',
-    version=__version__,
+    version=version,
     url='https://augus1999.github.io/mol2chemfigPy3/',
     description='python3 version of mol2chemfig',
     long_description=long_description,
@@ -23,6 +26,20 @@ setup(
     packages=find_packages(),
     python_requires='>=3.7',
     install_requires=['epam.indigo'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Chemistry",
+    ],
 )
 
 if os.path.exists('build'):
