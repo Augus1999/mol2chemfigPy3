82c340b73a7f3adf25700cfd5278f64e073bf8a2
Nianze TAO <39725660+Augus1999@users.noreply.github.com>
2025-12-27 13:31:21 +0900

    Updated test cases.

--- tests/test_read_file.py
+++ tests/test_read_file.py
@@ -1,20 +1,28 @@
 import os
+import platform
 from pathlib import Path
 import pytest
 from mol2chemfigPy3 import mol2chemfig
 
 
 cwd = Path(__file__).parent
+_sys = platform.system()
 
 target1 = r"""\chemfig{@{a7}HO-[@{a4-7},,2]@{a4}-[@{a3-4}:300]@{a3}-[@{a2-3}]@{a2}%
 -[@{a1-2}:60]@{a1}(-[:180,,,,draw=none]\mcfcringle{1.3})-[@{a1-6}:120]@{a6}%
 -[@{a5-6}:180]@{a5}(-[@{a4-5}:240])}"""
 target2 = r"""\chemfig{-[:180](-[:300,,,,draw=none]\mcfcringle{1.3})-[:240]-[:300]N-%
 -[:60](-[:120])}"""
-target3 = r"""\chemfig{@{a1}^{\mcfplus}H_3N-[@{a1-2}:6,,3]@{a2}-[@{a2-3}:306]@{a3}%
+if _sys != "Linux":
+    target3 = r"""\chemfig{@{a1}^{\mcfplus}H_3N-[@{a1-2}:6,,3]@{a2}-[@{a2-3}:306]@{a3}%
 -[@{a3-4}:6]@{a4}-[@{a4-5}:60]@{a5}(%
 -[:294,0.85,,,draw=none]\mcfcringle{1.03})-[@{a5-6}:348]@{a6}%
 -[@{a6-7}:276,,,1]@{a7}NH-[@{a7-8}:204,,1]@{a8}(-[@{a4-8}:132])}"""
+else:
+    target3 = r"""\chemfig{@{a1}^{\mcfplus}H_3N-[@{a1-2}:24,,3]@{a2}-[@{a2-3}:324]@{a3}%
+-[@{a3-4}:24]@{a4}-[@{a4-5}:330]@{a5}(%
+-[:96,0.85,,,draw=none]\mcfcringle{1.03})-[@{a5-6}:42]@{a6}%
+-[@{a6-7}:114]@{a7}\mcfabove{N}{H}-[@{a7-8}:186]@{a8}(-[@{a4-8}:258])}"""
 
 
 @pytest.mark.parametrize(
